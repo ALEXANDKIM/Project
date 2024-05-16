6e1ec8be15e0c4a08c5baedb93893bb5a1288beb
ALEXANDKIM <mancerakim68@gmail.com>
2024-05-16 12:14:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+/**
+ * Purpose: Handle the button click event to show a loader animation and redirect to secondpage.php
+ * Author: Alex Punay
+ * Contact: alexpunay@gmail.com
+ * Date: [Date of May 10, 2024]
+ * @param None
+ * @returns None
+ */
+
 // Add event listener to button
 document.getElementById("load-btn").addEventListener("click", function() {
   // Show loader
